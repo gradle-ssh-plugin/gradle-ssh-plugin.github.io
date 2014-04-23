7c4e58f644ecaca8c3c830fa49e0154041f95dc4
int128 <int128@gmail.com>
2014-04-23 23:56:06 +0900

    Put TOC into the sidebar

--- public/site.js
+++ public/site.js
@@ -1,7 +1,10 @@
 $(function () {
+  var toc = $('<span class="toc"/>').insertAfter('.sidebar-nav-item.active');
   $('h2[id],h3[id],h4[id],h5[id],h6[id]').each(function () {
-    $('.toc').append(
-      $('<div/>').append($('<a/>').attr('href', '#' + this.id).text($(this).text()))
-    );
+    toc.append(
+      $('<a class="sidebar-nav-item"/>')
+        .addClass('level-' + this.localName)
+        .attr('href', '#' + this.id)
+        .text($(this).text()));
   });
 });
